d9e55a308eda0bf91cd82a15b60dbba493aa5fdb
Tajiiii <daisuke.tajima.0218@gmail.com>
2020-09-15 17:02:43 +0900

    first commit

---
+++ app/controllers/customers_controller.rb
@@ -0,0 +1,13 @@
+class CustomersController < ApplicationController
+  def edit
+  end
+
+  def show
+  end
+
+  def withdraw
+  end
+
+  def unsubscribe
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,36 @@
+Rails.application.routes.draw do
+  devise_for :admins
+
+  namespace :admins do
+    get 'homes/top'
+    resources :orders,  only:[:index, :show, :update]
+    resources :order_details,  only:[:update]
+    resources :genres,  only:[:index, :create, :edit, :update]
+    resources :items,  only:[:index, :show, :new, :create, :edit, :update]
+  end
+
+  devise_for :customers
+
+  root'items#top'
+  resources :items, only: [:index, :show]
+  get 'items/top' => 'items#top', as: 'customer_top'
+  get 'items/about' => 'items#about', as: 'customer_about'
+
+  resources :customers, only: [:edit, :show, :update]
+    get 'customers/:id/regisration' => 'customers#regisration', as: 'customer_unregisration'
+    patch 'customers/:id/withdraw' => 'customers#withdraw', as: 'customer_withdraw'
+    put "/customers/:id/withdraw" => "customers#withdraw", as: 'customers_withdraw'
+  
+  resources :orders, only: [:new, :index, :create, :show]
+     post 'orders/confirm' => 'orders#confirm', as: 'order_confirm'
+     get 'orders/thanks' => 'orders#thanks', as: 'order_thanks'
+
+  resources :addresses, only: [:index, :create, :edit, :update, :destroy]
+
+  resources :cart_items, only: [:index, :create, :update, :destroy] do
+    collection do
+        delete 'destroy_all'
+    end
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
