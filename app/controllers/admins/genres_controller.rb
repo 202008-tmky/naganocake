edbadffd2a3d5f8af3ce9f1f0db2981c4b55a813
vagrant <vagrant@localhost.localdomain>
2020-09-16 07:03:14 +0000

    genre-index finish

--- app/controllers/admins/genres_controller.rb
+++ app/controllers/admins/genres_controller.rb
@@ -1,7 +1,21 @@
 class Admins::GenresController < ApplicationController
   def index
+	  @genres = Genre.all
+	  @genre = Genre.new
+  end
+
+  def create
+      @genre = Genre.new(genre_params)
+      @genre.save
+      redirect_to request.referer
   end
 
   def edit
+  end
+
+  private
+
+  def genre_params
+  	params.require(:genre).permit(:name, :is_active)
   end
 end
