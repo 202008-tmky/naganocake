9f946dfb328c2115b4cdc83c5078a3a30377c63b
vagrant <vagrant@localhost.localdomain>
2020-09-17 12:51:25 +0000

    [Add] items_index

--- app/controllers/admins/items_controller.rb
+++ app/controllers/admins/items_controller.rb
@@ -3,11 +3,33 @@ class Admins::ItemsController < ApplicationController
   end
 
   def index
+  	@items = Item.all.page(params[:page]).per(10)
   end
 
   def new
+  	@items = Item.new
+  end
+
+  def create
+  	@items = Item.new(item_params)
+    @items.save
+    redirect_to admins_item_path(@items)
   end
 
   def edit
+  	@item = Item.find(params[:id])
   end
+
+  def update
+  	@item = Item.find(params[:id])
+  	@item.update(item_params)
+  	redirect_to admins_item_path(@item)
+  end
+
+  private
+
+  def item_params
+  	params.require(:item).permit(:name, :image, :introduction, :price, :is_active, :genre_id)
+  end
+
 end
