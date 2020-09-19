e2222ad2f002f417fb01c39c685ad9c8bf9020d2
Tajiiii <daisuke.tajima.0218@gmail.com>
2020-09-19 13:11:53 +0900

    add customer-edit

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -1,6 +1,4 @@
 class Admins::CustomersController < ApplicationController
-  def edit
-  end
 
   def index
   	@customers = Customer.all
@@ -9,4 +7,21 @@ class Admins::CustomersController < ApplicationController
   def show
   	@customer = Customer.find(params[:id])
   end
+
+  def edit
+  	@customer = Customer.find(params[:id])
+  end
+
+  def update
+  	customer = Customer.find(params[:id])
+  	customer.update(customer_params)
+  	redirect_to admins_customer_path(customer)
+  end
+
+  private
+
+  def customer_params
+	params.require(:customer).permit(:email, :first_name, :first_name_kana, :last_name, :last_name_kana, :telephone_number, :postal_code, :address, :is_deleted)
+  end
+
 end
