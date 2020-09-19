8e41186bc08b325d06b0d8d75c02429305d803cd
Tajiiii <daisuke.tajima.0218@gmail.com>
2020-09-19 14:17:36 +0900

    finish customer-index show edit

--- app/controllers/admins/customers_controller.rb
+++ app/controllers/admins/customers_controller.rb
@@ -8,7 +8,7 @@ class Admins::CustomersController < ApplicationController
   	customer.update(customer_params)
   	redirect_to admins_customer_path(customer)
   end
-  
+
   def index
   	@customers = Customer.all.page(params[:page]).per(10)
   end
@@ -17,12 +17,10 @@ class Admins::CustomersController < ApplicationController
   	@customer = Customer.find(params[:id])
   end
 
-  
 
   private
 
   def customer_params
 	params.require(:customer).permit(:email, :first_name, :first_name_kana, :last_name, :last_name_kana, :telephone_number, :postal_code, :address, :is_deleted)
   end
-
 end
