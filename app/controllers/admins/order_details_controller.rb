15aec791a55845d47cf89b2526b9efee00d344c0
Tajiiii <daisuke.tajima.0218@gmail.com>
2020-09-21 17:14:58 +0900

    add order_details controller

---
+++ app/controllers/admins/order_details_controller.rb
@@ -0,0 +1,12 @@
+class Admins::OrderDetailsController < ApplicationController
+def update
+  	@order_detail = OrderDetail.find(params[:id])
+  	@order_detail.update(order_detail_params)
+  	redirect_back(fallback_location: admins_order_path)
+  end
+
+private
+ def order_detail_params
+ 	params.require(:order_detail).permit(:making_status)
+ end
+end
--- app/controllers/admins/orders_controller.rb
+++ app/controllers/admins/orders_controller.rb
@@ -3,5 +3,22 @@ class Admins::OrdersController < ApplicationController
   end
 
   def show
+  	@order = Order.find(params[:id])
+  	@orders = Order.all
+  	@order_details = OrderDetail.all
+  	@order_detail = OrderDetail.find(params[:id])
   end
+
+  def update
+  	@order = Order.find(params[:id])
+  	@order.update(order_params)
+  	redirect_back(fallback_location: admins_order_path)
+  end
+
+private
+ def order_params
+ 	params.require(:order).permit(:status)
+
+ end
 end
+
